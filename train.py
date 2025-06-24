73463c434ccde0290a61932bed105694f27d3a4b
Leo Cheung <yzhang252@dxc.com>
2025-06-24 11:36:31 +0800

    Update train.py

--- train.py
+++ train.py
@@ -8,7 +8,8 @@ import time
 from minbpe import BasicTokenizer, RegexTokenizer
 
 # open some text and train a vocab of 512 tokens
-text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
+#text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
+text = open("data/TinyStoriesV2-GPT4-train.txt", "r", encoding="utf-8").read()
 
 # create a directory for models, so we don't pollute the current directory
 os.makedirs("models", exist_ok=True)
@@ -18,6 +19,11 @@ for TokenizerClass, name in zip([BasicTokenizer, RegexTokenizer], ["basic", "reg
 
     # construct the Tokenizer object and kick off verbose training
     tokenizer = TokenizerClass()
+
+    # train方法参数说明：
+    # text: 训练用的文本数据
+    # 512: 词汇表大小，即要生成的token数量
+    # verbose=True: 是否显示训练过程的详细信息
     tokenizer.train(text, 512, verbose=True)
     # writes two files in the models directory: name.model, and name.vocab
     prefix = os.path.join("models", name)
