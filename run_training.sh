3f88db6b4b2fbd6aea4ce8df2e0546b23708ebc0
yyzhang <zhangyaoyi2011@gmail.com>
2025-06-26 15:53:01 +0800

    dev

---
+++ run_training.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run the training script with nohup in the background
+nohup /home/overman/workspaces/stanford-cs336/minbpe/.conda/bin/python /home/overman/workspaces/stanford-cs336/minbpe/train.py > training.log 2>&1 &
+
+# Print the process ID
+echo "Training started with PID: $!"
+echo "Output is being logged to training.log" 
