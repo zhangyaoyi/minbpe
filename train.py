04bb1291803a34954ef731fb920c6e44f6d74188
yyzhang <zhangyaoyi2011@gmail.com>
2025-06-26 17:37:58 +0800

    Refactor dataset handling in train.py to dynamically set vocabulary size and model name based on selected dataset

--- train.py
+++ train.py
@@ -12,12 +12,19 @@ from minbpe.regex import GPT4_SPLIT_PATTERN
 
 # create a directory for models, so we don't pollute the current directory
 os.makedirs("models", exist_ok=True)
-
 t0 = time.time()
-# open some text and train a vocab of 512 tokens
-#text = open("tests/taylorswift.txt", "r", encoding="utf-8").read()
-#text = open("data/owt_train.txt", "r", encoding="utf-8").read()
-text = open("data/TinyStoriesV2-GPT4-train.txt", "r", encoding="utf-8").read()
+
+dataset = "tiny"
+
+if dataset == "owt":
+    text = open("data/owt_train.txt", "r", encoding="utf-8").read()
+    vocab_size = 32000
+    model_name = "regex_32k"
+elif dataset == "tiny":
+    text = open("data/TinyStoriesV2-GPT4-train.txt", "r", encoding="utf-8").read()
+    vocab_size = 10000
+    model_name = "regex_10k"
+
 t1 = time.time()
 print(f"Loaded text in {t1 - t0:.2f} seconds")
 
@@ -26,11 +33,11 @@ tokenizer = RegexTokenizer(GPT4_SPLIT_PATTERN)
 tokenizer.register_special_tokens({"<|endoftext|>": 100257})
 
 print(f"Training tokenizer")
-tokenizer.train(text, 10000, verbose=True)
+tokenizer.train(text, vocab_size, verbose=True)
 t2 = time.time()
 print(f"Trained tokenizer in {t2 - t1:.2f} seconds")
 # writes two files in the models directory: name.model, and name.vocab
-prefix = os.path.join("models", "regex_10k")
+prefix = os.path.join("models", model_name)
 tokenizer.save(prefix)
 t3 = time.time()
 print(f"Saved tokenizer in {t3 - t2:.2f} seconds")
