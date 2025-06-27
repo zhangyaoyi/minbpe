fc47b1da2ddad5f2eb1199fd9a96aebfa3bf6d52
yyzhang <zhangyaoyi2011@gmail.com>
2025-06-27 15:34:12 +0800

    Update run_training.sh to execute train_tokenizer.py instead of train.py for training process

--- run_training.sh
+++ run_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the training script with nohup in the background
-nohup /home/overman/workspaces/stanford-cs336/minbpe/.conda/bin/python /home/overman/workspaces/stanford-cs336/minbpe/train.py > training.log 2>&1 &
+nohup /home/overman/workspaces/stanford-cs336/minbpe/.conda/bin/python /home/overman/workspaces/stanford-cs336/minbpe/train_tokenizer.py > training.log 2>&1 &
 
 # Print the process ID
 echo "Training started with PID: $!"
